8ea29ca8ed318d29feb7c8cd71ef808df36b0107
Onahail <mahavemann@gmail.com>
2015-11-12 11:14:11 -0500

    Cleaned up rally point panorama code

--- content/dota_addons/iQueue/panorama/scripts/iqueue/rally_point.js
+++ content/dota_addons/iQueue/panorama/scripts/iqueue/rally_point.js
@@ -3,13 +3,14 @@
 var RallyTable = RallyTable || {};	
 
 
-
-function InitializeRallyTable( event ){
-	
-	var index = event.entindex;
-	
-	$.Schedule( 0.1, function(){
-		
+/*
+Setup
+------------------------------------------------------------------------------------------------------
+*/
+function InitializeRallyTable( event )
+{
+	var index = event.entindex;	
+	$.Schedule( 0.1, function(){	
 		if (FindUnitLabel(index, "CanRally"))
 		{
 			$.Msg("Initialziing flag table for index:", index)	
@@ -20,6 +21,100 @@ function InitializeRallyTable( event ){
 		}
 	});
 }
+//------------------------------------------------------------------------------------------------------
+
+/*
+Main rally point functionality
+----------------------------------------------------------------------------------------------------------
+*/
+function DetermineSelection()
+{
+	var playerID = Players.GetLocalPlayer();
+	var mainSelected = Players.GetLocalPlayerPortraitUnit();
+	var	selectedEntities = Players.GetSelectedEntities( playerID );
+	if (FindUnitLabel(mainSelected, "CanRally"))
+	{
+		$("#RallyButton").SetHasClass( "_hidden", false);
+	}
+	else
+	{
+		$("#RallyButton").SetHasClass( "_hidden", true);	
+	}
+	
+	if (Entities.IsControllableByPlayer( mainSelected, Game.GetLocalPlayerID()))
+	{
+		for (var index in RallyTable)
+		{
+			if (ArrayContains(selectedEntities, index))
+			{
+				ShowRallyPoint(index, playerID)
+			}
+			else
+			{
+				HideRallyPoint(index, playerID)
+			}
+		}
+	}
+}
+function SetRallyPoint()
+{
+	var playerID = Players.GetLocalPlayer();
+	var mainSelected = Players.GetLocalPlayerPortraitUnit();
+	var selectedEntities = Players.GetSelectedEntities( playerID );
+	var mouseLocation = GameUI.GetScreenWorldPosition( GameUI.GetCursorPosition());
+	var targetEntity = GetRallyPointTarget()
+
+	for (i = 0; i < selectedEntities.length; i++)
+	{
+		if (FindUnitLabel( selectedEntities[i], "CanRally" ))
+		{	
+			//$.Msg("Setting rally for:", selectedEntities[i])
+			if (targetEntity != -1)
+			{
+				// Player set rally point on an entity
+				//$.Msg("Target is an entity: ", Entities.GetUnitName( targetEntity ))
+				
+				GameEvents.SendCustomGameEventToServer( "player_set_rally_point_entity", {entIndex : selectedEntities[i], targetEntity : targetEntity});
+				RallyTable[selectedEntities[i]].rallySet = true;
+				RallyTable[selectedEntities[i]].rallyOnEntity = true;
+				RallyTable[selectedEntities[i]].targetEntity = targetEntity;
+				RallyTable[selectedEntities[i]].buildingLocation = Entities.GetAbsOrigin(selectedEntities[i])
+				ShowRallyPoint(selectedEntities[i], playerID)
+			}
+			else
+			{
+				// Player set rally point on ground
+				GameEvents.SendCustomGameEventToServer( "player_set_rally_point_ground", {entIndex : selectedEntities[i], targetPoint : mouseLocation});
+				RallyTable[selectedEntities[i]].rallySet = true;
+				RallyTable[selectedEntities[i]].rallyOnEntity = false;
+				RallyTable[selectedEntities[i]].flagLocation = mouseLocation;
+				RallyTable[selectedEntities[i]].buildingLocation = Entities.GetAbsOrigin(selectedEntities[i])
+				ShowRallyPoint(selectedEntities[i], playerID)
+			}
+		}
+	}
+}
+function RemoveRallyPoint()
+{
+	var playerID = Players.GetLocalPlayer();
+	var selectedEntities = Players.GetSelectedEntities( playerID );
+	
+	for (i = 0; i < selectedEntities.length; i++)
+	{
+		if (RallyTable[selectedEntities[i]].rallySet == true)
+		{
+			HideRallyPoint(selectedEntities[i], playerID, false)
+			GameEvents.SendCustomGameEventToServer( "player_removed_rally_point", {entIndex : selectedEntities[i]});
+			RallyTable[selectedEntities[i]].rallySet = false;
+		}
+	}
+}
+//-----------------------------------------------------------------------------------------------------------------------------
+
+/*
+Mouse Selection functions
+----------------------------------------------------------------------------------------------
+*/
 
 GameUI.SetMouseCallback( function( eventName, arg ) {
 	var nMouseButton = arg
@@ -63,52 +158,72 @@ function GetRallyPointTarget()
 	{
 		return e.entityIndex;
 	}
-
 	return -1;
 }
+//-----------------------------------------------------------------------------------------------------------
 
-
-
-
-function SetRallyPoint()
+/*
+Rally point UI button contol
+-------------------------------------------------------------------------------------------------------------
+*/
+function LeftClickRallyButton()
 {
+	//$.Msg("Rally button pressed")
 	var playerID = Players.GetLocalPlayer();
 	var mainSelected = Players.GetLocalPlayerPortraitUnit();
 	var selectedEntities = Players.GetSelectedEntities( playerID );
-	var mouseLocation = GameUI.GetScreenWorldPosition( GameUI.GetCursorPosition());
-	var targetEntity = GetRallyPointTarget()
-
+	RallyTable.RallyButtonPressed = true;
+	RallyTable[mainSelected].rallySet = false;
 	for (i = 0; i < selectedEntities.length; i++)
 	{
-		if (FindUnitLabel( selectedEntities[i], "CanRally" ))
-		{	
-			//$.Msg("Setting rally for:", selectedEntities[i])
-			if (targetEntity != -1)
+		if (FindUnitLabel( selectedEntities[i], "CanRally" ) == false)
+		{
+			var a = selectedEntities.indexOf(selectedEntities[i]);
+			if (a != -1)
 			{
-				// Player set rally point on an entity
-				//$.Msg("Target is an entity: ", Entities.GetUnitName( targetEntity ))
-				
-				GameEvents.SendCustomGameEventToServer( "player_set_rally_point_entity", {entIndex : selectedEntities[i], targetEntity : targetEntity});
-				RallyTable[selectedEntities[i]].rallySet = true;
-				RallyTable[selectedEntities[i]].rallyOnEntity = true;
-				RallyTable[selectedEntities[i]].targetEntity = targetEntity;
-				RallyTable[selectedEntities[i]].buildingLocation = Entities.GetAbsOrigin(selectedEntities[i])
-				ShowRallyPoint(selectedEntities[i], playerID)
-			}
-			else
-			{
-				// Player set rally point on ground
-				GameEvents.SendCustomGameEventToServer( "player_set_rally_point_ground", {entIndex : selectedEntities[i], targetPoint : mouseLocation});
-				RallyTable[selectedEntities[i]].rallySet = true;
-				RallyTable[selectedEntities[i]].rallyOnEntity = false;
-				RallyTable[selectedEntities[i]].flagLocation = mouseLocation;
-				RallyTable[selectedEntities[i]].buildingLocation = Entities.GetAbsOrigin(selectedEntities[i])
-				ShowRallyPoint(selectedEntities[i], playerID)
-			}
+				selectedEntities.splice(a, 1);
+			}	
 		}
 	}
+	for (i = 0; i < selectedEntities.length; i++)
+	{
+		RallyTable[selectedEntities[i]].rallySet = false;
+		$.Msg(RallyTable[mainSelected].rallySet)
+		if (RallyTable[selectedEntities[i]].flag != undefined)
+		{
+			Particles.DestroyParticleEffect(RallyTable[selectedEntities[i]].flag, true);
+			Particles.DestroyParticleEffect(RallyTable[selectedEntities[i]].line, true);
+			RallyTable[selectedEntities[i]].flag = undefined;
+		}
+		RallyTable[selectedEntities[i]].flag = Particles.CreateParticle("particles/iqueue_particles/rally_flag.vpcf", ParticleAttachment_t.PATTACH_CUSTOMORIGIN, playerID)
+		RallyTable[selectedEntities[i]].line = Particles.CreateParticle("particles/units/heroes/hero_wisp/wisp_tether.vpcf", ParticleAttachment_t.PATTACH_CUSTOMORIGIN, playerID);
+		Particles.SetParticleControl(RallyTable[selectedEntities[i]].line, 1, Entities.GetAbsOrigin(selectedEntities[i]))
+		Particles.SetParticleControl(RallyTable[selectedEntities[i]].line, 2, Entities.GetAbsOrigin(selectedEntities[i]))
+	}
+	var b = selectedEntities.indexOf(mainSelected);
+	if (b != -1)
+	{
+		selectedEntities.splice(b, 1);
+	}
+	KeepFlagOnMouse(mainSelected, selectedEntities)
 }
 
+function RightClickRallyButton()
+{
+	bRallyPointActive = true;
+	var contextMenu = $.CreatePanel( "DOTAContextMenuScript", $.GetContextPanel(), "" );
+	contextMenu.AddClass( "ContextMenu_NoArrow" );
+	contextMenu.AddClass( "ContextMenu_NoBorder" );
+	contextMenu.GetContentsPanel().SetHasClass( "bRallyPointActive", bRallyPointActive );
+	contextMenu.GetContentsPanel().BLoadLayout( "file://{resources}/layout/custom_game/rally_point_context_menu.xml", false, false );
+	contextMenu.GetContentsPanel().data().ParentPanel = $("#RallyButton");
+}
+//------------------------------------------------------------------------------------------------------------------------
+
+/*
+Particle control
+-------------------------------------------------------------------------------------------------------------------------
+*/
 function ShowRallyPoint(index, playerID, entityTargetRally)
 {
 	if (RallyTable[index].line != undefined)
@@ -145,21 +260,6 @@ function ShowRallyPoint(index, playerID, entityTargetRally)
 		}
 	}
 }
-
-function KeepLineOnEntity(index)
-{
-	var playerID = Players.GetLocalPlayer();
-	var mainSelected = Players.GetLocalPlayerPortraitUnit();
-
-	var targetLocation = Entities.GetAbsOrigin(RallyTable[index].targetEntity);
-	if ( targetLocation !== null && RallyTable[index].rallyOnEntity == true && RallyTable[index].line != undefined)
-	{
-		Particles.SetParticleControl(RallyTable[index].line, 0, targetLocation); 
-		$.Schedule(1/60, function(){KeepLineOnEntity(index);})
-	}
-}
-
-
 function HideRallyPoint(index, playerID)
 {
 	if (RallyTable[index].line != undefined)
@@ -173,94 +273,6 @@ function HideRallyPoint(index, playerID)
 		RallyTable[index].flag = undefined
 	}
 }
-
-
-function DetermineSelection()
-{
-	
-	var playerID = Players.GetLocalPlayer();
-	var mainSelected = Players.GetLocalPlayerPortraitUnit();
-	var	selectedEntities = Players.GetSelectedEntities( playerID );
-	
-	if (FindUnitLabel(mainSelected, "CanRally"))
-	{
-		$("#RallyButton").SetHasClass( "_hidden", false);
-	}
-	else
-	{
-		$("#RallyButton").SetHasClass( "_hidden", true);	
-	}
-	
-	if (Entities.IsControllableByPlayer( mainSelected, Game.GetLocalPlayerID()))
-	{
-		for (var index in RallyTable)
-		{
-			if (ArrayContains(selectedEntities, index))
-			{
-				ShowRallyPoint(index, playerID)
-			}
-			else
-			{
-				HideRallyPoint(index, playerID)
-			}
-		}
-	}
-}
-
-function LeftClickRallyButton()
-{
-
-	$.Msg("Rally button pressed")
-	
-	var playerID = Players.GetLocalPlayer();
-	var mainSelected = Players.GetLocalPlayerPortraitUnit();
-	var selectedEntities = Players.GetSelectedEntities( playerID );
-	
-	RallyTable.RallyButtonPressed = true;
-	RallyTable[mainSelected].rallySet = false;
-	
-	for (i = 0; i < selectedEntities.length; i++)
-	{
-		if (FindUnitLabel( selectedEntities[i], "CanRally" ) == false)
-		{
-			var a = selectedEntities.indexOf(selectedEntities[i]);
-			if (a != -1)
-			{
-				selectedEntities.splice(a, 1);
-			}	
-		}
-	}
-	
-	for (i = 0; i < selectedEntities.length; i++)
-	{
-		RallyTable[selectedEntities[i]].rallySet = false;
-		$.Msg(RallyTable[mainSelected].rallySet)
-		if (RallyTable[selectedEntities[i]].flag != undefined)
-		{
-			Particles.DestroyParticleEffect(RallyTable[selectedEntities[i]].flag, true);
-			Particles.DestroyParticleEffect(RallyTable[selectedEntities[i]].line, true);
-			RallyTable[selectedEntities[i]].flag = undefined;
-		}
-		RallyTable[selectedEntities[i]].flag = Particles.CreateParticle("particles/iqueue_particles/rally_flag.vpcf", ParticleAttachment_t.PATTACH_CUSTOMORIGIN, playerID)
-		RallyTable[selectedEntities[i]].line = Particles.CreateParticle("particles/units/heroes/hero_wisp/wisp_tether.vpcf", ParticleAttachment_t.PATTACH_CUSTOMORIGIN, playerID);
-		Particles.SetParticleControl(RallyTable[selectedEntities[i]].line, 1, Entities.GetAbsOrigin(selectedEntities[i]))
-		Particles.SetParticleControl(RallyTable[selectedEntities[i]].line, 2, Entities.GetAbsOrigin(selectedEntities[i]))
-	}
-	
-	var b = selectedEntities.indexOf(mainSelected);
-	if (b != -1)
-	{
-		selectedEntities.splice(b, 1);
-	}
-	
-
-	//$.Msg(mainSelected)
-	//$.Msg(selectedEntities)
-	//$.Msg(RallyTable[mainSelected].rallySet)
-	KeepFlagOnMouse(mainSelected, selectedEntities)
-	
-}
-
 function KeepFlagOnMouse(mainSelected, selectedEntities)
 {
 	
@@ -282,43 +294,27 @@ function KeepFlagOnMouse(mainSelected, selectedEntities)
 		$.Schedule(1/60, function(){KeepFlagOnMouse(mainSelected, selectedEntities)});
 	}
 }
-
-
-
-
-
-
-function RightClickRallyButton()
-{
-	
-	bRallyPointActive = true;
-	
-	var contextMenu = $.CreatePanel( "DOTAContextMenuScript", $.GetContextPanel(), "" );
-	contextMenu.AddClass( "ContextMenu_NoArrow" );
-	contextMenu.AddClass( "ContextMenu_NoBorder" );
-	contextMenu.GetContentsPanel().SetHasClass( "bRallyPointActive", bRallyPointActive );
-	contextMenu.GetContentsPanel().BLoadLayout( "file://{resources}/layout/custom_game/rally_point_context_menu.xml", false, false );
-	contextMenu.GetContentsPanel().data().ParentPanel = $("#RallyButton");
-	
-}
-
-function RemoveRallyPoint()
+function KeepLineOnEntity(index)
 {
 	var playerID = Players.GetLocalPlayer();
-	var selectedEntities = Players.GetSelectedEntities( playerID );
-	
-	for (i = 0; i < selectedEntities.length; i++)
+	var mainSelected = Players.GetLocalPlayerPortraitUnit();
+
+	var targetLocation = Entities.GetAbsOrigin(RallyTable[index].targetEntity);
+	if ( targetLocation !== null && RallyTable[index].rallyOnEntity == true && RallyTable[index].line != undefined)
 	{
-		if (RallyTable[selectedEntities[i]].rallySet == true)
-		{
-			HideRallyPoint(selectedEntities[i], playerID, false)
-			GameEvents.SendCustomGameEventToServer( "player_removed_rally_point", {entIndex : selectedEntities[i]});
-			RallyTable[selectedEntities[i]].rallySet = false;
-		}
+		Particles.SetParticleControl(RallyTable[index].line, 0, targetLocation); 
+		$.Schedule(1/60, function(){KeepLineOnEntity(index);})
 	}
 }
+//------------------------------------------------------------------------------------------------------------------------
 
 
+
+
+/*
+Misc Functions
+------------------------------------------------------------------
+*/
 function ArrayContains(array, query)
 {
 	for (i = 0; i < array.length; i++)
@@ -342,7 +338,7 @@ function FindUnitLabel( index, queryLabel )
 		}else{
 			return false;}
 }
-
+//-----------------------------------------------------------------
 
 
 
